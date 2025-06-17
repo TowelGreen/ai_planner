0b4999a15e7083f5b223c3390fc126c1dbaef892
TowelGreen <bunhop005@gmail.com>
2025-06-16 23:30:15 -0700

    fixed input and sidebar

--- src/pages/LocalPlanner.tsx
+++ src/pages/LocalPlanner.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React from 'react'
+import '../styles/Local.module.css';
 
 const LocalPlanner: React.FC = () => {
   return (
     <div style={{ textAlign: 'center', paddingTop: '100px' }}>
       <h2>📍 Local Date Planner</h2>
       <p>This is where local planning features will go.</p>
+      <label className = "custom-field">
+        <input type = "text"
+        placeholder='Enter Zip'/>
+        <span className = "placeholder"> </span>
+      </label>
     </div>
+    
   );
 };
 
